df01b21c9b020969f5e6d3fc6e89b7d83ce0b731
Henry McIntosh <henrymcintosh@henrys-air.home>
2021-07-01 23:51:57 +0100

    add watch mode

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const {Command, flags} = require('@oclif/command')
 const path = require('path')
 const midwife = require('midwife')
 const Plugins = require('midwife-core-plugins')
+const chokidar = require('chokidar');
 
 class MidwifeCliCommand extends Command {
   async run() {
     const {flags} = this.parse(MidwifeCliCommand)
-    const {file} = flags
+    const {file, watch} = flags
     const fileName = path.resolve(process.cwd(), file)
     let config
     try {
@@ -22,7 +23,26 @@ class MidwifeCliCommand extends Command {
         plugins: Plugins.all,
       }
     }
-    midwife.build(config)
+    if (watch) {
+      const dirs = [path.resolve(process.cwd(), config.dataDir), path.resolve(process.cwd(), config.viewsDir)]
+      let running = false;
+      chokidar.watch(dirs).on("all", () => {
+        if (running) return;
+        running = true;
+        console.log("Building...")
+        midwife.build(config)
+        .then(() => {
+          running = false;
+          console.log("Waiting for changes...")
+        })
+        .catch( () => {
+          running = false;
+          console.log("Waiting for changes...")
+        })
+      })
+    } else {
+      midwife.build(config)
+    }
   }
 }
 
@@ -35,6 +55,7 @@ MidwifeCliCommand.flags = {
   // add --help flag to show CLI version
   help: flags.help({char: 'h'}),
   file: flags.string({char: 'f', description: 'Build config to use', default: 'midwife.config.js'}),
+  watch: flags.boolean({char: 'w', description: 'Watch the data and views directory for changes', default: false}),
 }
 
 module.exports = MidwifeCliCommand
